b4a402d310bc3a299627de76a135e53c9b8c975f
Your Name <vipa4ka@gmail.com>
2022-11-02 17:47:42 +0200

    hbl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import './css/index.css';
+import cardTpl from './templates/cards.hbs';
+import './styles.css';
+import './images/sprite.svg'
+import cards from './menu.json';
+
+console.log(cardTpl(cards[0]));
 
 
+// const listEl = document.querySelector('js-menu');
+// const renderCards=onRenderCards(cards)
+// listEl.insertAdjacentHTML("beforebegin", renderCards);
+
+// function onRenderCards(img) {
+//     img.map((e) => {
+//         console.log(e);
+        
+        
+//     })
+    
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const HandlebarsPlugin = require("handlebars-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -13,20 +13,48 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/i,
-       use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        test: /\.css$/i,
+       use: ["style-loader", "css-loader"],
       },
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: ['babel-loader']
       }, 
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]',
+              limit: 8192,
+              esModule: false,
+            },
+          },
+          'img-loader',
+        ],
+      },
+      
+      // {
+      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,        
+      //   use: 'raw-loader',
+      // },
+      
+      {
+        test: /\.hbs$/,
+        use: 'handlebars-loader',
+      },
+    
+      
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new MiniCssExtractPlugin(),
+    // new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
+   
+    
   ],
   devServer: { 
     port: 9000,
