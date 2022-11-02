088604acd7c17ed49df9d2f429e2e92099bb553a
Your Name <vipa4ka@gmail.com>
2022-11-02 12:00:14 +0200

    init project

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new MiniCssExtractPlugin({}),
+    new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
   ],
   devServer: { 
